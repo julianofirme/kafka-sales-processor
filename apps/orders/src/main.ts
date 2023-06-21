278b35eb7b5725a320495bc1f9f466afe4d7b18a
Juliano <julianofirme23@gmail.com>
2023-06-21 12:57:55 +0000

    chore: convert to monorepo app

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(OrdersModule);
   await app.listen(3000);
 }
 bootstrap();
